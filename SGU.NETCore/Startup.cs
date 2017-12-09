f42217d1a38b2ccf51fc41f6aa5d7c6e427a18fa
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-12-09 18:42:18 -0500

    Test .net core

--- SGU.NETCore/Startup.cs
+++ SGU.NETCore/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace SGU.NETCore
 {
@@ -15,7 +17,12 @@ namespace SGU.NETCore
     {
         public Startup(IConfiguration configuration)
         {
+            string[] firstNames = { "juan", "carlos", "pepe", "paola", "ricardo" };
+            firstNames.Select(name => name.Firts().ToString().ToUpper() + name.Substring(1));
+
             Configuration = configuration;
+                                  
+            FieldInfo runtimeInfo = PlatformServices.Default.Application.RuntimeFramework.GetType().GetField("_theRuntime");
         }
 
         public IConfiguration Configuration { get; }
@@ -23,18 +30,51 @@ namespace SGU.NETCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                    .AddSessionStateTempDataProvider();
+            services.AddSession(options => {
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Name = "Sinco";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSession();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
+
+            string str = "L";
+            int cost;
+            switch(str){
+                case "S":
+                    cost = 1;
+                    break;
+                case "M";
+                    cost = 2;
+                    break;
+                case "L":
+                    cost = 3;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid size");
+            }
         }
+    }
+
+    public class Person{
+        
+        public string FirstName { get; set;}
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string FullName{ get => $"{LastName}, {FirstName}"; }
     }
 }
